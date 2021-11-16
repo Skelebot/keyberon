f77dd9e928b2b814d4a42e1dcea3ec5aacc3e057
Antoni Simka <antonisimka.8@gmail.com>
2021-11-16 19:37:21 +0000

    Add colon symbol to layout macro

--- keyberon-macros/src/keycodes.rs
+++ keyberon-macros/src/keycodes.rs
@@ -28,6 +28,7 @@ pub fn punctuation_to_keycode(p: &Punct, out: &mut TokenStream) {
         '<' => out.extend(quote! { keyberon::action::Action::MultipleKeyCodes(&[keyberon::key_code::KeyCode::LShift, keyberon::key_code::KeyCode::Comma]), }),
         '>' => out.extend(quote! { keyberon::action::Action::MultipleKeyCodes(&[keyberon::key_code::KeyCode::LShift, keyberon::key_code::KeyCode::Dot]), }),
         '?' => out.extend(quote! { keyberon::action::Action::MultipleKeyCodes(&[keyberon::key_code::KeyCode::LShift, keyberon::key_code::KeyCode::Slash]), }),
+        ':' => out.extend(quote! { keyberon::action::Action::MultipleKeyCodes(&[keyberon::key_code::KeyCode::LShift, keyberon::key_code::KeyCode::SColon]), }),
         // Is this reachable?
         _ => emit_error!(p, "Punctuation could not be parsed as a keycode")
     }
